dccc38d67a458f8334824b32e667f14743ce2238
andole98 <andole87@gmail.com>
2020-02-03 23:34:51 +0900

    test: 몽고 bulk insert
    - 컬렉션 insert : 150 ms
    - insertOne : 16000 ms

--- src/test/java/com/andole/mongo/Mongo.java
+++ src/test/java/com/andole/mongo/Mongo.java
@@ -2,30 +2,56 @@ package com.andole.mongo;
 
 import com.andole.mongo.domain.Person;
 import com.mongodb.client.MongoClients;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
-import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 public class Mongo {
     private static final Logger log = getLogger(Mongo.class);
+    private MongoTemplate mongoTemplate;
+
+    @BeforeEach
+    void setUp() {
+        mongoTemplate = new MongoTemplate(MongoClients.create(), "database");
+    }
 
     @Test
-    void insertAndFind() {
-        MongoOperations mongoOps = new MongoTemplate(MongoClients.create(), "database");
+    void insert() {
+        clockWatch(this::insertOne);
+        clockWatch(this::collections);
+    }
 
+    private void clockWatch(Runnable task) {
         long start = System.currentTimeMillis();
-
-        for (int i = 0; i < 10000; i++) {
-            log.info(mongoOps.insert(new Person("andole", i)).toString());
-        }
-
+        task.run();
         long end = System.currentTimeMillis();
         log.error("duration: {}", end - start);
-        log.info(mongoOps.findOne(new Query(where("name").is("andole")), Person.class).toString());
+    }
+
+    void insertOne() {
+        for (int i = 0; i < 10000; i++) {
+            mongoTemplate.insert(new Person("andole", i));
+        }
+    }
+
+    void collections() {
+        List<Person> idbb = IntStream.range(0, 10000)
+                .mapToObj(i -> new Person("idbb", i))
+                .collect(Collectors.toList());
+        mongoTemplate.insertAll(idbb);
+    }
+
+    @Test
+    void find() {
+        mongoTemplate.find(new Query(where("name").is("idbb")), Person.class);
     }
 }
